7e1a481294c752acd0ae16c5b702f7c42213783d
Daniel Bravo <140561569+DANIELBRAVO21@users.noreply.github.com>
2023-10-24 15:15:03 -0500

    Delete JV-Peliculabarbie.js

--- JV-Peliculabarbie.js
+++
@@ -1,9 +0,0 @@
-const boxes = document.querySelectorAll('.box');
-
-boxes.forEach(box => {
-    box.addEventListener('click', () => {
-        // Toggle the 'active' class to show/hide the video
-        box.classList.toggle('active');
-    });
-});
-
